0912ddcf9b3990a9cfcf3d3312490ac7979ef730
Andrey <andrey.batasov@yahoo.com>
2025-11-09 13:28:18 +0100

    Configure GitHub Pages deployment with custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // For custom domain (localbird.ch)
   server: {
     host: "::",
     port: 8080,
